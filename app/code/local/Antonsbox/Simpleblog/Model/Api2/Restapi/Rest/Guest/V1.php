3c5a7ed8341481dd19b2f9e54a5b9bf4e3f73104
antonsbox <antonsbox2010@gmail.com>
2016-09-24 17:13:40 +0300

    fixing _retrieve

--- app/code/local/Antonsbox/Simpleblog/Model/Api2/Restapi/Rest/Guest/V1.php
+++ app/code/local/Antonsbox/Simpleblog/Model/Api2/Restapi/Rest/Guest/V1.php
@@ -3,34 +3,30 @@
 class Antonsbox_Simpleblog_Model_Api2_Restapi_Rest_Guest_V1
     extends Antonsbox_Simpleblog_Model_Api2_Restapi
 {
-//    public function _retrieveCollection()
-//    {
-//        //get order Collection
-//        return $res;
-//    }
-//
-//    public function _multiUpdate($data)
-//    {
-//        //do all the business logic magic in here.
-//
-//        $this->_successMessage('OK', Mage_Api2_Model_Server::HTTP_OK, array('item' => $result));
-//        if ($error) {
-//            $this->_errorMessage('Orders not found', Mage_Api2_Model_Server::HTTP_NOT_FOUND, array('item' => $error));
-//        }
-//    }
-
-//    protected function _retrieve()
-//    {
-//        $registryCollection = Mage::getModel('simpleblog/post')->getCollection();
-//        return $registryCollection;
-//    }
-
-
     public function _retrieve(array $data)
     {
+        $param= $this->getRequest()->getParam('id');
+        Mage::log('param='.$param);
+        $collection = Mage::getModel("simpleblog/post")->getCollection();
+        $collection->addFieldToFilter('post_id', array('eq' => $param));
+        $outputArray = array();
+        foreach ($collection as $item) {
+            $postId = $item->getPostId();
+            $title = $item->getTitle();
+            $content = $item->getContent();
+            $created = $item->getCreated();
+            $post_to_encode = array();
+            $post_to_encode[] = array(
+                'post_id' => $postId,
+                'title' => $title,
+                'content' => $content,
+                'created' => $created);
+            array_push($outputArray, $post_to_encode);
+        }
 
-
-        return json_encode('YEEES!!! It`s a _retrive!!');
+        Mage::log($outputArray);
+        $ecoded_data = json_encode($outputArray);
+        return $ecoded_data;
     }
 
     public function _retrieveCollection()
@@ -43,10 +39,9 @@ class Antonsbox_Simpleblog_Model_Api2_Restapi_Rest_Guest_V1
             $title = $item->getTitle();
             $content = $item->getContent();
             $created = $item->getCreated();
-
             $post_to_encode = array();
             $post_to_encode[] = array(
-                'id' => $postId,
+                'post_id' => $postId,
                 'title' => $title,
                 'content' => $content,
                 'created' => $created);
@@ -58,43 +53,43 @@ class Antonsbox_Simpleblog_Model_Api2_Restapi_Rest_Guest_V1
 
     public function _delete(array $data)
     {
+        Mage::log('_delete');
         return json_encode('YEEES!!! It`s a _delete!!');
+    }
+
+    public function _multiDelete(array $data)
+    {
+        Mage::log('_multiDelete');
+        return json_encode('YEEES!!! It`s a _multiDelete!!');
     }
 
     public function _update(array $data)
     {
+        Mage::log('_update');
         return json_encode('YEEES!!! It`s a _update!!');
     }
 
+    public function _multiUpdate(array $data)
+    {
+        Mage::log('_multiUpdate');
+        return json_encode('YEEES!!! It`s a _multiUpdate!!');
+    }
 
     public function _create(array $data)
     {
-        $currentTimestamp = Mage::getModel('core/date')->timestamp(time());
-//        $postId = $data['id'];
-//        $postTitle = $data['title'];
-//        $postContent = $data['content'];
-//        $postCreated = $data['created'];
-//        $post->setId($postId);
-//        $post->setTitle($postTitle);
-//        $post->setContent($postContent);
-        foreach ($data as $item) {
-            $post = Mage::getModel("simpleblog/post");
-            $post->setCreated(date('Y-m-d H:i:s', $currentTimestamp));
-            $post->setTitle($item[0]);
-            $post->setContent('CONTENT FROM API');
-            $post->save();
-            Mage::log($item);
-        }
-
+        $post = Mage::getModel("simpleblog/post");
+        $post->setCreated($data['created']);
+        $post->setTitle($data['title']);
+        $post->setContent($data['content']);
+        $post->save();
+        Mage::log($data);
         return $this->_getLocation($post);
     }
 
     public function _multiCreate(array $data)
     {
-        $currentTimestamp = Mage::getModel('core/date')->timestamp(time());
         foreach ($data as $item) {
             $post = Mage::getModel("simpleblog/post");
-//            $post->setCreated(date('Y-m-d H:i:s', $currentTimestamp));
             $post->setCreated($item['created']);
             $post->setTitle($item['title']);
             $post->setContent($item['content']);
